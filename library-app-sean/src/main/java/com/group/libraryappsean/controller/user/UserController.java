63d06077351796890598dcdb87770a6691d97e6e
synk2577 <tjdus2577@gmail.com>
2023-09-14 00:32:02 +0900

    feat: add jdbc to api about user

--- library-app-sean/src/main/java/com/group/libraryappsean/controller/user/UserController.java
+++ library-app-sean/src/main/java/com/group/libraryappsean/controller/user/UserController.java
@@ -3,31 +3,62 @@ package com.group.libraryappsean.controller.user;
 import com.group.libraryappsean.domain.user.User;
 import com.group.libraryappsean.dto.user.request.UserCreateRequest;
 import com.group.libraryappsean.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
+//    private final List<User> users = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate; // JdbcTemplate: jdbc에 대한 커넥터
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        // jdbcTemplate 를 생성자에 직접 넣지 않더라도 Spring이 알아서 jdbcTemplate을 넣어줌
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user") // POST /user - 유저 생성
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+//        users.add(new User(request.getName(), request.getAge()));
+        String sql = "insert into user (name, age) values (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user") // GET /user - 유저 조회
     public List<UserResponse> getUsers() {
-        List<UserResponse> responses = new ArrayList<>();
-        for (int i = 0; i < users.size(); i++) {
-            responses.add(new UserResponse(i + 1, users.get(i)));
-        }
+//        List<UserResponse> responses = new ArrayList<>();
+//        for (int i = 0; i < users.size(); i++) {
+//            responses.add(new UserResponse(i + 1, users.get(i)));
+//        }
+//
+//        return responses;
 
-        return responses;
+        String sql = "select * from user";
+//        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+//            @Override
+//            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                long id = rs.getLong("id");
+//                String name = rs.getString("name");
+//                int age = rs.getInt("age");
+//                return new UserResponse(id, name, age);
+//            }
+//        });
+
+        // 위 코드를 java lambda를 사용해 간결하게!
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
     }
 }
