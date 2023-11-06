992769ba8c5131d76adec7628219bf9f286b4215
synk2577 <tjdus2577@gmail.com>
2023-11-06 23:22:09 +0900

    study: add put, delete api about user

---
+++ library-app-sean/src/main/java/com/group/libraryappsean/dto/user/request/UserUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.group.libraryappsean.dto.user.request;
+
+public class UserUpdateRequest {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
